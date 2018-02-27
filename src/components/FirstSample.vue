f1d9d17c837b02bfb8ef101cac3c63d2cdda7e0e
kokorau <kokorau@gmail.com>
2018-02-28 01:31:19 +0900

    shader取り込むの成功!

--- src/components/FirstSample.vue
+++ src/components/FirstSample.vue
@@ -14,10 +14,11 @@ export default {
     // === renderer ===
     const renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setPixelRatio( window.devicePixelRatio)
 
     // === camera ===
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.z = 5
+    const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 3000)
+    camera.position.z = 4
 
     // === light ===
     const light1 = new THREE.DirectionalLight(0xffffff)
@@ -26,14 +27,54 @@ export default {
     // === model ===
     const geometry = new THREE.BoxBufferGeometry(0.75, 0.75, 0.75)
 
+    const uniforms = {
+      time: { value: 1.0 }
+    }
+
+    const vertexShader = `
+    varying vec2 vUv;
+    void main()
+    {
+      vUv = uv;
+      vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+      gl_Position = projectionMatrix * mvPosition;
+    }
+    `
+    const fragmentShader = `
+    uniform float time;
+    varying vec2 vUv;
+    void main(void) {
+      vec2 p = - 1.0 + 2.0 * vUv;
+      float a = time * 40.0;
+      float d, e, f, g = 1.0 / 40.0 ,h ,i ,r ,q;
+      e = 400.0 * ( p.x * 0.5 + 0.5 );
+      f = 400.0 * ( p.y * 0.5 + 0.5 );
+      i = 200.0 + sin( e * g + a / 150.0 ) * 20.0;
+      d = 200.0 + cos( f * g / 2.0 ) * 18.0 + cos( e * g ) * 7.0;
+      r = sqrt( pow( abs( i - e ), 2.0 ) + pow( abs( d - f ), 2.0 ) );
+      q = f / r;
+      e = ( r * cos( q ) ) - a / 2.0;
+      f = ( r * sin( q ) ) - a / 2.0;
+      d = sin( e * g ) * 176.0 + sin( e * g ) * 164.0 + r;
+      h = ( ( f + d ) + a / 2.0 ) * g;
+      i = cos( h + r * p.x / 1.3 ) * ( e + e + a ) + cos( q * g * 6.0 ) * ( r + h / 3.0 );
+      h = sin( f * g ) * 144.0 - sin( e * g ) * 212.0 * p.x;
+      h = ( h + ( f - e ) * q + sin( r - ( a + h ) / 7.0 ) * 10.0 + i / 4.0 ) * g;
+      i += cos( h * 2.3 * sin( a / 350.0 - q ) ) * 184.0 * sin( q - ( r * 4.3 + a / 12.0 ) * g ) + tan( r * g + h ) * 184.0 * cos( r * g + h );
+      i = mod( i / 5.6, 256.0 ) / 64.0;
+      if ( i < 0.0 ) i += 4.0;
+      if ( i >= 2.0 ) i = 4.0 - i;
+      d = r / 350.0;
+      d += sin( d * d * 8.0 ) * 0.52;
+      f = ( sin( a * g ) + 1.0 ) / 2.0;
+      gl_FragColor = vec4( vec3( f * i / 1.6, i / 2.0 + d / 13.0, i ) * d * p.x + vec3( i / 1.3 + d / 8.0, i / 2.0 + d / 18.0, i ) * d * ( 1.0 - p.x ), 1.0 );
+    }
+    `
+
     const material = new THREE.ShaderMaterial({
-      uniforms: {
-        time: {value: 1.0},
-        vertexShader: require('./vertexShader.glsl'),
-        // vertexShader: require('./vertexShader.txt'),
-        fragmentShader: require('./fragmentShader.glsl')
-        // fragmentShader: require('./fragmentShader.txt')
-      }
+      uniforms: uniforms,
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader
     })
 
     const cube = new THREE.Mesh(geometry, material)
@@ -46,12 +87,14 @@ export default {
       renderer: renderer,
       camera: camera,
       lights: {
-        light1
+        // light1
       },
       objects: {
         cube: cube
       },
-      clock: clock
+      clock: clock,
+
+      uniforms: uniforms
     }
   },
 
@@ -73,6 +116,7 @@ export default {
   },
 
   mounted () {
+    window.addEventListener('resize', this.onWindowResize)
     // === DOMを追加, animate ===
     const stage = document.getElementById('stage')
     stage.appendChild(this.renderer.domElement)
@@ -90,18 +134,25 @@ export default {
     render () {
       const delta = this.clock.getDelta()
 
+      this.uniforms.time.value += delta * 10
+
       Object.keys(this.objects).forEach((key) => {
         this.objects[key].rotation.x += delta * 1.2
         this.objects[key].rotation.y += delta * 1.2
       })
 
       this.renderer.render(this.scene, this.camera)
+    },
+
+    onWindowResize (event) {
+      this.camera.aspect = window.innerWidth / window.innerHeight
+      this.camera.updateProjectionMatrix()
+
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
     }
 
   }
 }
 </script>
 
-<style lang="sass">
-
-</style>
+<style lang="sass"></style>
